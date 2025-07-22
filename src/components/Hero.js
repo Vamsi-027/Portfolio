ae649f1f59fe8d77a7085a15eedecf56e5384d40
Cheruku Vamsi <65002479+Vamsi-027@users.noreply.github.com>
2025-07-22 11:09:15 -0500

    Fix JSX syntax error in Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -36,7 +36,7 @@ function Hero() {
     >
       {/* Background Pattern */}
       <div className="absolute inset-0 opacity-10">
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg%20width=\"60\"%20height=\"60\"%20viewBox=\"0%200%2060%2060\"%20xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg%20fill=\"none\"%20fill-rule=\"evenodd\"%3E%3Cg%20fill=\"%23ffffff\"%20fill-opacity=\"0.1\"%3E%3Ccircle%20cx=\"30\"%20cy=\"30\"%20r=\"2\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg%20width=%2260%22%20height=%2260%22%20viewBox=%220%200%2060%2060%22%20xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cg%20fill=%22none%22%20fill-rule=%22evenodd%22%3E%3Cg%20fill=%22%23ffffff%22%20fill-opacity=%220.1%22%3E%3Ccircle%20cx=%2230%22%20cy=%2230%22%20r=%222%22/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
       </div>
 
       <div className="text-center px-4 relative z-10 max-w-4xl mx-auto">
