bd92bff4cd25dd2b4ff2e53624cb4526ca71fd07
vamsi-rebounz <vamsi.cheruku@rebounz.org>
2026-03-04 21:23:08 -0600

    feat: Replace multiple raster favicons and apple touch icons with a single SVG icon.

--- app/layout.js
+++ app/layout.js
@@ -86,9 +86,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body 
